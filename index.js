177675c3acb9df90bcdcd484d0d85fb7f89c35f1
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 07:21:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments) {
   var array = [ ]
- // for (let i = 0; i < musicians.length + 1; i += 1 ){
- let i = 0
+ for (let i = 0; i < musicians.length + 1; i += 1 ){
     var string = musicians[i] + " plays " + instruments[i]
     array.push(string)
   return array
+ }
 }
